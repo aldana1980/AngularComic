34dd46e76678a7d70bcb2efccc2c819bcde7cd30
aldana1980 <programaionjava@gmail.com>
2020-06-22 23:44:22 -0300

    Primer commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+
+})
+export class HeroesComponent implements OnInit {
+
+  constructor( private_heroesService: HeroesService ) {
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
